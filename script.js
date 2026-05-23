22f0b1f1dcca8e1d059e35d2d934a815c3067013
Krithi Prakash <19736krithi@gmail.com>
2026-05-23 11:44:55 +0530

    Refactor task addition functionality
    Refactor addTask function to use event listener and simplify task creation.

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
+const addBtn = document.getElementById("addBtn");
+
 const taskInput = document.getElementById("taskInput");
+
 const taskList = document.getElementById("taskList");
 
-function addTask() {
+addBtn.addEventListener("click", function () {
 
-    const taskText = taskInput.value.trim();
+    const task = taskInput.value;
 
-    if (taskText === "") {
+    if (task === "") {
+        alert("Enter a task");
         return;
     }
 
     const li = document.createElement("li");
 
-    li.innerHTML = `
-        <div class="task-left">
-            <input type="checkbox">
-            <span>${taskText}</span>
-        </div>
-
-        <button class="delete-btn">Delete</button>
-    `;
-
-    const checkbox = li.querySelector("input");
-    const deleteBtn = li.querySelector(".delete-btn");
-
-    checkbox.addEventListener("change", () => {
-        li.classList.toggle("completed");
-    });
-
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
-    });
+    li.innerText = task;
 
     taskList.appendChild(li);
 
     taskInput.value = "";
-}
+
+});
